b21addb16cbc2e64cf386b8a875b8b6e421ecd30
Tripleo Nova <you@figureitout.com>
2022-10-07 19:49:12 -0400

    (semi) useless FunctionalInterface annotation

--- src/main/java/tripleo/elijah/stages/deduce/OnGenClass.java
+++ src/main/java/tripleo/elijah/stages/deduce/OnGenClass.java
@@ -2,6 +2,7 @@ package tripleo.elijah.stages.deduce;
 
 import tripleo.elijah.stages.gen_fn.GeneratedClass;
 
+@FunctionalInterface
 public interface OnGenClass {
 	void accept(final GeneratedClass aGeneratedClass);
 }
