6dc37952c7cc161440515bec9fbc751979bd1d36
Tripleo Nova <you@figureitout.com>
2022-10-07 18:19:01 -0400

    fix stupidity

--- src/main/java/tripleo/elijah/util/Stupidity.java
+++ src/main/java/tripleo/elijah/util/Stupidity.java
@@ -2,9 +2,9 @@ package tripleo.elijah.util;
 
 public class Stupidity {
 	public static void println_err(String aS) {
-		System.err.println(aS);
+//		System.err.println(aS);
 	}
 	public static void println_out(String aS) {
-		System.out.println(aS);
+//		System.out.println(aS);
 	}
 }
