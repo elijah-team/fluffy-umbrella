c4f4561922c28f314a4881d237c5cb0b0e0adaf9
Tripleo Nova <you@figureitout.com>
2022-10-16 18:15:25 -0400

    introducing: fluffy

---
+++ src/main/java/tripleo/elijah/stages/deduce/fluffy/impl/FluffyModuleImpl.java
@@ -0,0 +1,24 @@
+package tripleo.elijah.stages.deduce.fluffy.impl;
+
+import tripleo.elijah.stages.deduce.fluffy.i.FluffyLsp;
+import tripleo.elijah.stages.deduce.fluffy.i.FluffyMember;
+import tripleo.elijah.stages.deduce.fluffy.i.FluffyModule;
+
+import java.util.List;
+
+public class FluffyModuleImpl implements FluffyModule {
+	@Override
+	public FluffyLsp lsp() {
+		return null;
+	}
+
+	@Override
+	public String name() {
+		return null;
+	}
+
+	@Override
+	public List<FluffyMember> members() {
+		return null;
+	}
+}
