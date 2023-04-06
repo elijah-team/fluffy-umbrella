22604f1217fed8c3b53b9a089b68916c1dcdb466
Tripleo Nova <you@figureitout.com>
2023-04-06 19:05:40 -0400

    [refactor/feature] replace GPL with DoubleLatch
    - GeneratePipeline Latch

--- src/main/java/tripleo/elijah/comp/GeneratePipeline.java
+++ src/main/java/tripleo/elijah/comp/GeneratePipeline.java
@@ -8,79 +8,56 @@
  */
 package tripleo.elijah.comp;
 
-import org.jdeferred2.DoneCallback;
-import org.jetbrains.annotations.Contract;
 import tripleo.elijah.comp.i.IPipelineAccess;
+
 import tripleo.elijah.stages.gen_fn.EvaNode;
+
 import tripleo.elijah.stages.gen_generic.DoubleLatch;
 import tripleo.elijah.stages.gen_generic.GenerateResult;
 import tripleo.elijah.stages.gen_generic.GenerateResultItem;
 import tripleo.elijah.stages.gen_generic.pipeline_impl.DefaultGenerateResultSink;
+
 import tripleo.elijah.util.NotImplementedException;
 
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Contract;
+
 /**
  * Created 8/21/21 10:16 PM
  */
 public class GeneratePipeline implements PipelineMember, Consumer<Supplier<GenerateResult>> {
-	private final Compilation c;
+	//private final Compilation               c;
+	private final DefaultGenerateResultSink grs;
 
 	@Contract(pure = true)
-	public GeneratePipeline(IPipelineAccess pa) {
-		c     = pa.getCompilation();
-		latch = new GPL(pa);
+	public GeneratePipeline(@NotNull IPipelineAccess pa) {
+		//c     = pa.getCompilation();
+		grs   = new DefaultGenerateResultSink(this, pa);
+
+		latch2 = new DoubleLatch<List<EvaNode>>(nodes -> {
+			pa.pipelineLogic().generate(nodes, grs);
+
+			final List<GenerateResultItem> x = grs.resultList();
+			int                            y = 2;
+		});
 
 		final DeducePipeline deducePipeline = pa.getDeducePipeline();
 
-		deducePipeline.lgcp(new DoneCallback<List<EvaNode>>() {
-			@Override
-			public void onDone(final List<EvaNode> a_lgc) {
-				latch.set(a_lgc);
-			}
-		});
-
-		final GPL latch3 = new GPL(pa);
-
-		latch2 = new DoubleLatch<List<EvaNode>>(nodes -> {
-			latch.run();
-		});
+		deducePipeline.lgcp(latch2::notify);
 	}
-
-	final private GPL latch;
 
 	@Override
 	public void run() {
-		//latch.run(); //see latch2.run() above
+		latch2.notify(true);
 	}
 
 	@Override
 	public void accept(Supplier<GenerateResult> t) {
 		NotImplementedException.raise();
-	}
-
-	private class GPL implements Runnable {
-		private List<EvaNode>             result;
-		private DefaultGenerateResultSink grs;
-
-		private GPL(final IPipelineAccess pa) {
-			grs = new DefaultGenerateResultSink(GeneratePipeline.this, pa);
-		}
-
-		@Contract(mutates = "this")
-		public void set(final List<EvaNode> aResult) {
-			result = aResult;
-		}
-
-		@Override
-		public void run() {
-			c.pipelineLogic.generate(result, grs);
-
-			final List<GenerateResultItem> x = grs.resultList();
-			int                            y = 2;
-		}
 	}
 
 	private final DoubleLatch<List<EvaNode>> latch2;
