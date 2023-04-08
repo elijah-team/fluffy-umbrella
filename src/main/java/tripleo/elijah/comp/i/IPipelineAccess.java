5c035b16d755bc6ab130787af31d5f288690e823
Tripleo Nova <you@figureitout.com>
2023-04-08 00:45:24 -0400

    oops: add ProcessRecord and pipelinePromise accessors to IPipelineAccess

--- src/main/java/tripleo/elijah/comp/i/IPipelineAccess.java
+++ src/main/java/tripleo/elijah/comp/i/IPipelineAccess.java
@@ -1,8 +1,11 @@
 package tripleo.elijah.comp.i;
 
+import org.jdeferred2.Promise;
+
 import tripleo.elijah.comp.Compilation;
 import tripleo.elijah.comp.DeducePipeline;
 import tripleo.elijah.comp.PipelineLogic;
+import tripleo.elijah.comp.ProcessRecord;
 
 public interface IPipelineAccess {
 	Compilation getCompilation();
@@ -10,4 +13,8 @@ public interface IPipelineAccess {
 	DeducePipeline getDeducePipeline();
 
 	PipelineLogic pipelineLogic();
+
+	ProcessRecord getProcessRecord();
+
+	Promise<PipelineLogic, Void, Void> getPipelineLogicPromise();
 }
