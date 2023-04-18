b89d2178e050697488c6784b8dc8966225a60fc3
Tripleo Nova BP <you@youfigureitout.com>
2023-04-18 07:42:11 -0400

    [introduce] ZonePath

--- src/main/java/tripleo/elijah/stages/gen_c/Zone.java
+++ src/main/java/tripleo/elijah/stages/gen_c/Zone.java
@@ -2,9 +2,14 @@ package tripleo.elijah.stages.gen_c;
 
 import tripleo.elijah.stages.gen_fn.BaseEvaFunction;
 import tripleo.elijah.stages.gen_fn.VariableTableEntry;
+import tripleo.elijah.stages.instructions.IdentIA;
+import tripleo.elijah.stages.instructions.InstructionArgument;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static tripleo.elijah.stages.gen_c.CReference._getIdentIAPathList;
 
 class Zone {
 	private final Map<Object, ZoneMember> members = new HashMap<Object, ZoneMember>();
@@ -15,6 +20,17 @@ class Zone {
 
 		final ZoneVTE r = new ZoneVTE__1(aVarTableEntry, aGf);
 		members.put(aVarTableEntry, r);
+		return r;
+	}
+
+	public ZonePath getPath(final IdentIA aIdentIA) {
+		final List<InstructionArgument> s = _getIdentIAPathList(aIdentIA);
+
+		if (members.containsKey(aIdentIA))
+			return (ZonePath) members.get(aIdentIA);
+
+		final ZonePath r = new ZonePath(aIdentIA, s);
+		members.put(aIdentIA, r);
 		return r;
 	}
 }
