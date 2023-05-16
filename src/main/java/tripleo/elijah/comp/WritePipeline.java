39c465b941789a351e14040bb9a81b07f8f07455
Tripleo Nova BP <you@youfigureitout.com>
2023-05-16 17:18:31 -0400

    save results of all ops in flow to class member

--- src/main/java/tripleo/elijah/comp/WritePipeline.java
+++ src/main/java/tripleo/elijah/comp/WritePipeline.java
@@ -50,12 +50,13 @@ import static tripleo.elijah.util.Helpers.List_of;
 /**
  * Created 8/21/21 10:19 PM
  */
-public class WritePipeline implements PipelineMember, @NotNull Consumer<Supplier<GenerateResult>>, AB_GenerateResultListener {
+public class WritePipeline implements PipelineMember, Consumer<Supplier<GenerateResult>>, AB_GenerateResultListener {
 	public final  WritePipelineSharedState                   st;
 	public final  DeferredObject<GenerateResult, Void, Void> prom = new DeferredObject<>();
 	private final CompletedItemsHandler                      cih;
-	private final DoubleLatch<GenerateResult>                latch;
-	private       Supplier<GenerateResult>                   grs;
+	private final DoubleLatch<GenerateResult>                                              latch;
+	private       HashMap<WP_Indiviual_Step, Pair<WP_Flow.FlowStatus, Operation<Boolean>>> ops;
+	private       Supplier<GenerateResult>                                                 grs;
 
 
 	public WritePipeline(final @NotNull IPipelineAccess pa) {
@@ -65,9 +66,6 @@ public class WritePipeline implements PipelineMember, @NotNull Consumer<Supplier
 
 		// given
 		st.c = pa.getCompilation();
-
-		final @NotNull ProcessRecord                      pr  = pa.getProcessRecord();
-		final @NotNull Promise<PipelineLogic, Void, Void> ppl = pa.getPipelineLogicPromise();
 
 		// computed
 		st.file_prefix = new File("COMP", st.c.getCompilationNumberString());
@@ -82,20 +80,13 @@ public class WritePipeline implements PipelineMember, @NotNull Consumer<Supplier
 			final WP_Indiviual_Step wpis_wf = new WPIS_WriteFiles(this);
 			final WP_Indiviual_Step wpis_wb = new WPIS_WriteBuffers(this);
 
-			Operation<Boolean> op;
-
 			// TODO: Do something with op, like set in {@code pa} to proceed to next pipeline
-			try {
-				final WP_Flow f = new WP_Flow(List_of(wpis_go, wpis_mk, wpis_wi, wpis_wf, wpis_wb));
-				// TODO WP_FlowMember?
-				// TODO each IndividualStep may return an op?
-				//  - with type or Boolean?
-				//  - are we modeing effects here?
-				f.act();
-				op = Operation.success(true);
-			} catch (Exception aE) {
-				op = Operation.failure(aE);
-			}
+			final WP_Flow f = new WP_Flow(List_of(wpis_go, wpis_mk, wpis_wi, wpis_wf, wpis_wb));
+			// TODO WP_FlowMember?
+			// TODO each IndividualStep may return an op?
+			//  - with type or Boolean?
+			//  - are we modeing effects here?
+			ops = f.act();
 		});
 
 		// state
@@ -169,14 +160,24 @@ public class WritePipeline implements PipelineMember, @NotNull Consumer<Supplier
 	}
 
 	class WP_Flow {
+		enum FlowStatus {
+			NOT_TRIED, TRIED, FAILED
+		}
+
+		private final HashMap<WP_Indiviual_Step, Pair<FlowStatus, Operation<Boolean>>> ops = new HashMap<WP_Indiviual_Step, Pair<FlowStatus, Operation<Boolean>>>();
+
 		private final List<WP_Indiviual_Step> steps = new ArrayList<>();
 
 		WP_Flow(final Collection<? extends WP_Indiviual_Step> s) {
 			steps.addAll(s);
 		}
 
-		void act() throws Exception {
+		HashMap<WP_Indiviual_Step, Pair<FlowStatus, Operation<Boolean>>> act() {
 			final WP_State_Control_1 sc = new WP_State_Control_1();
+
+			for (final WP_Indiviual_Step step : steps) {
+				ops.put(step, Pair.of(FlowStatus.NOT_TRIED, null));
+			}
 
 			for (final WP_Indiviual_Step step : steps) {
 				sc.clear();
@@ -184,9 +185,14 @@ public class WritePipeline implements PipelineMember, @NotNull Consumer<Supplier
 				step.act(st, sc);
 
 				if (sc.hasException()) {
-					throw sc.getException();
+					ops.put(step, Pair.of(FlowStatus.FAILED, Operation.failure(sc.getException())));
+					break;
+				} else {
+					ops.put(step, Pair.of(FlowStatus.TRIED, Operation.success(true)));
 				}
 			}
+
+			return ops;
 		}
 	}
 
