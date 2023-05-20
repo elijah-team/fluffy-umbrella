780f7f53950989be65911aeaf326cbb80bea3aaa
Tripleo Nova BP <you@youfigureitout.com>
2023-05-20 15:41:57 -0400

    add note

--- src/main/java/tripleo/elijah/stages/deduce/post_bytecode/DeduceElement3_Type.java
+++ src/main/java/tripleo/elijah/stages/deduce/post_bytecode/DeduceElement3_Type.java
@@ -5,6 +5,9 @@ import tripleo.elijah.nextgen.query.Operation2;
 import tripleo.elijah.stages.gen_fn.GenType;
 import tripleo.elijah.stages.gen_fn.TypeTableEntry;
 
+/**
+ * Also {@link tripleo.elijah.stages.deduce.post_bytecode.DeduceType3}
+ */
 public interface DeduceElement3_Type {
 	TypeTableEntry typeTableEntry();
 
