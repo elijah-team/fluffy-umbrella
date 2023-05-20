f35e234f34da161036a338e6ab7b81d925f26534
Tripleo Nova BP <you@youfigureitout.com>
2023-05-20 15:41:19 -0400

    reduce volume

--- src/main/java/tripleo/elijah/comp/internal/CD_CompilationRunnerStart_1.java
+++ src/main/java/tripleo/elijah/comp/internal/CD_CompilationRunnerStart_1.java
@@ -19,28 +19,21 @@ public class CD_CompilationRunnerStart_1 implements CD_CompilationRunnerStart {
 					  final boolean do_out,
 					  final @NotNull IPipelineAccess pa) {
 		try {
-			_____start((CompilerInstructionsImpl) aCompilerInstructions, do_out, cr, pa);
+			final CB_Output out = new CB_Output();
+
+			final CR_FindCIs              f1 = new CR_FindCIs(pa.getCompilerInput());
+			final CR_ProcessInitialAction f2 = new CR_ProcessInitialAction((CompilerInstructionsImpl) aCompilerInstructions, do_out);
+			final CR_AlmostComplete       f3 = new CR_AlmostComplete(cr);
+			final CR_RunBetterAction      f4 = new CR_RunBetterAction();
+
+			final @NotNull List<CR_Action> l = List_of(f1, f2, f3, f4);
+
+			for (final CR_Action each : l) {
+				each.execute(cr.crState, out);
+			}
 		} catch (Exception aE) {
 			final CompilationClosure ccl = pa.getCompilationClosure();
 			ccl.errSink().exception(aE);
-		}
-	}
-
-	public void _____start(final CompilerInstructionsImpl ci,
-							final boolean do_out,
-							final @NotNull CompilationRunner cr,
-							final @NotNull IPipelineAccess pa) throws Exception {
-		final CB_Output out = new CB_Output();
-
-		final CR_FindCIs              f1 = new CR_FindCIs(pa.getCompilerInput());
-		final CR_ProcessInitialAction f2 = new CR_ProcessInitialAction(cr, ci, do_out);
-		final CR_AlmostComplete       f3 = new CR_AlmostComplete(cr);
-		final CR_RunBetterAction      f4 = new CR_RunBetterAction();
-
-		final @NotNull List<CR_Action> l = List_of(f1, f2, f3, f4);
-
-		for (final CR_Action each : l) {
-			each.execute(cr.crState, out);
 		}
 	}
 }
