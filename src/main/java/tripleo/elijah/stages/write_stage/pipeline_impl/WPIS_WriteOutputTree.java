593183aa5c0b839c0663258fa7b1dc33f5715467
Tripleo Nova BP <you@youfigureitout.com>
2023-05-20 13:22:00 -0400

    add logs
    - HACK

--- src/main/java/tripleo/elijah/stages/write_stage/pipeline_impl/WPIS_WriteOutputTree.java
+++ src/main/java/tripleo/elijah/stages/write_stage/pipeline_impl/WPIS_WriteOutputTree.java
@@ -1,21 +1,25 @@
 package tripleo.elijah.stages.write_stage.pipeline_impl;
 
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import tripleo.elijah.comp.Compilation;
-import tripleo.elijah.comp.WritePipeline;
-import tripleo.elijah.comp.functionality.f203.F203;
+import tripleo.elijah.nextgen.outputstatement.EG_Naming;
+import tripleo.elijah.nextgen.outputstatement.EG_SequenceStatement;
+import tripleo.elijah.nextgen.outputstatement.EG_SingleStatement;
 import tripleo.elijah.nextgen.outputstatement.EG_Statement;
 import tripleo.elijah.nextgen.outputtree.EOT_OutputFile;
 import tripleo.elijah.nextgen.outputtree.EOT_OutputTree;
+import tripleo.elijah.nextgen.outputtree.EOT_OutputType;
+import tripleo.elijah.stages.logging.ElLog;
+import tripleo.elijah.stages.logging.LogEntry;
 import tripleo.util.io.CharSink;
 import tripleo.util.io.FileCharSink;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
+
+import static tripleo.elijah.util.Helpers.List_of;
 
 public class WPIS_WriteOutputTree implements WP_Indiviual_Step {
 	@Override
@@ -23,22 +27,50 @@ public class WPIS_WriteOutputTree implements WP_Indiviual_Step {
 		final EOT_OutputTree       ot = st.c.getOutputTree();
 		final List<EOT_OutputFile> l  = ot.getList();
 
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		// HACK should be done earlier in process
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		final List<ElLog> logs = st.pa.getCompilationEnclosure().getPipelineLogic().elLogs;
+		final String s1 = logs.get(0).getFileName();
+
+		for (final ElLog log : logs) {
+			final List<EG_Statement> stmts = new ArrayList<>();
+
+			if (log.getEntries().size() == 0) continue;
+
+			for (final LogEntry entry : log.getEntries()) {
+				final String logentry = String.format("[%s] [%tD %tT] %s %s", s1, entry.time, entry.time, entry.level, entry.message);
+				stmts.add(new EG_SingleStatement(logentry));
+			}
+
+			final EG_SequenceStatement seq      = new EG_SequenceStatement(new EG_Naming("wot.log.seq"), stmts);
+			final String               fileName = new String(log.getFileName()).replace("/", "~~");
+			final EOT_OutputFile       off      = new EOT_OutputFile(st.c, List_of(), fileName, EOT_OutputType.LOGS, seq);
+			l.add(off);
+		}
+
 		for (final EOT_OutputFile outputFile : l) {
 			final String       path0 = outputFile.getFilename();
 			String             path;
 			final EG_Statement seq   = outputFile.getStatementSequence();
 
 			switch (outputFile.getType()) {
-			case SOURCES -> {
-				path = MessageFormat.format("COMP/{0}/code/{1}", st.base_dir, path0);
-			}
-			case LOGS -> {
-				path = MessageFormat.format("COMP/{0}/logs/{1}", st.base_dir, path0);
-			}
-			case INPUTS -> {
-				path = MessageFormat.format("COMP/{0}/inputs.txt", st.base_dir);
-			}
-			default -> path = path0;
+			case SOURCES -> path = MessageFormat.format("{0}/code/{1}", st.base_dir, path0);
+			case LOGS 	 -> path = MessageFormat.format("{0}/logs/{1}", st.base_dir, path0);
+			case INPUTS  -> path = MessageFormat.format("{0}/inputs.txt", st.base_dir);
+			default 	 -> path = path0;
 			}
 
 			System.out.println("401 Writing path: " + path);
@@ -53,12 +85,5 @@ public class WPIS_WriteOutputTree implements WP_Indiviual_Step {
 					((FileCharSink) x).close();
 			}
 		}
-	}
-
-	private @NotNull File choose_dir_name(final @NotNull Compilation c) {
-		final File fn00 = new F203(c.getErrSink(), c).chooseDirectory();
-		final File fn01 = new File(fn00, "code");
-
-		return fn01;
 	}
 }
