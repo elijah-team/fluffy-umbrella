a90e16952192c863b75b7951feaa3c3faa1f8742
Tripleo Sat LP <largepoppa@tripleo.milkyway>
2023-09-16 16:08:29 -0400

    refactor: this is pretier

--- src/main/java/tripleo/elijah/comp/USE.java
+++ src/main/java/tripleo/elijah/comp/USE.java
@@ -1,23 +1,35 @@
 package tripleo.elijah.comp;
 
-import org.jetbrains.annotations.*;
-import tripleo.elijah.ci.*;
-import tripleo.elijah.comp.caches.*;
-import tripleo.elijah.comp.diagnostic.*;
-import tripleo.elijah.comp.specs.*;
-import tripleo.elijah.diagnostic.*;
-import tripleo.elijah.lang.*;
-import tripleo.elijah.nextgen.query.*;
-import tripleo.elijah.util.*;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import tripleo.elijah.ci.CompilerInstructions;
+import tripleo.elijah.ci.CompilerInstructionsImpl;
+import tripleo.elijah.ci.GenerateStatement;
+import tripleo.elijah.ci.LibraryStatementPart;
+import tripleo.elijah.ci.LibraryStatementPartImpl;
+import tripleo.elijah.comp.caches.DefaultElijahCache;
+import tripleo.elijah.comp.diagnostic.ExceptionDiagnostic;
+import tripleo.elijah.comp.diagnostic.FileNotFoundDiagnostic;
+import tripleo.elijah.comp.specs.ElijahCache;
+import tripleo.elijah.comp.specs.ElijahSpec;
+import tripleo.elijah.diagnostic.Diagnostic;
+import tripleo.elijah.lang.OS_Module;
+import tripleo.elijah.lang.StringExpression;
+import tripleo.elijah.nextgen.query.Mode;
+import tripleo.elijah.nextgen.query.Operation2;
+import tripleo.elijah.util.Helpers;
 
-import java.io.*;
-import java.util.*;
-import java.util.regex.*;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.regex.Pattern;
 
 @SuppressWarnings("UnnecessaryLocalVariable")
 class USE {
-	private final        Compilation            c;
-	private final        ErrSink                errSink;
+	private final Compilation c;
+	private final ErrSink     errSink;
 
 	private static final FilenameFilter accept_source_files = (directory, file_name) -> {
 		final boolean matches = Pattern.matches(".+\\.elijah$", file_name)
@@ -54,37 +66,76 @@ class USE {
 		lsp.setInstructions(compilerInstructions);
 		use_internal(instruction_dir, do_out, lsp);
 	}
+
 	private final ElijahCache elijahCache = new DefaultElijahCache();
 
+	static class CY_FindPrelude {
+		private final ErrSink errSink;
+		private final USE     x;
+
+		CY_FindPrelude(final ErrSink aErrSink1, final USE aX) {
+			errSink = aErrSink1;
+			x       = aX;
+		}
+
+		public Operation2<OS_Module> findPrelude(final String prelude_name) {
+			final File local_prelude = local_prelude_file(prelude_name);
+
+			if (!(local_prelude.exists())) {
+				return Operation2.failure(new FileNotFoundDiagnostic(local_prelude));
+			}
+
+			try {
+				return try_parse(local_prelude);
+			} catch (final Exception e) {
+				errSink.exception(e);
+				return Operation2.failure(new ExceptionDiagnostic(e));
+			}
+		}
+
+		@NotNull
+		private Operation2<OS_Module> try_parse(final File local_prelude) {
+			Operation2<OS_Module>      om;
+
+			try {
+				om = x.realParseElijjahFile(local_prelude.getName(), local_prelude, false);
+
+				switch (om.mode()) {
+				case SUCCESS -> {
+					final CompilerInstructions instructions = new CompilerInstructionsImpl();
+					instructions.setName("prelude");
+					final GenerateStatement generateStatement = new GenerateStatement();
+					final StringExpression  expression        = new StringExpression(Helpers.makeToken("\"c\"")); // TODO
+					generateStatement.addDirective(Helpers.makeToken("gen"), expression);
+					instructions.add(generateStatement);
+					final LibraryStatementPart lsp = new LibraryStatementPartImpl();
+					lsp.setInstructions(instructions);
+					//lsp.setDirName();
+					final OS_Module module = om.success();
+					module.setLsp(lsp);
+					return Operation2.success(module);
+				}
+				case FAILURE -> {
+					return om;
+				}
+				default -> throw new IllegalStateException("Unexpected value: " + om.mode());
+				}
+
+			} catch (final Exception aE) {
+				om = Operation2.failure(new ExceptionDiagnostic(aE));
+			}
+
+			return om;
+		}
+
+		@NotNull
+		private static File local_prelude_file(final String prelude_name) {
+			return new File("lib_elijjah/lib-" + prelude_name + "/Prelude.elijjah");
+		}
+	}
+
 	public Operation2<OS_Module> findPrelude(final String prelude_name) {
-		final File local_prelude = new File("lib_elijjah/lib-" + prelude_name + "/Prelude.elijjah");
-
-		if (!(local_prelude.exists())) {
-			return Operation2.failure(new FileNotFoundDiagnostic(local_prelude));
-		}
-
-		try {
-			final Operation2<OS_Module> om = realParseElijjahFile2(local_prelude.getName(), local_prelude, false);
-
-			assert om.mode() == Mode.SUCCESS;
-
-			final CompilerInstructions instructions = new CompilerInstructionsImpl();
-			instructions.setName("prelude");
-			final GenerateStatement generateStatement = new GenerateStatement();
-			final StringExpression  expression        = new StringExpression(Helpers.makeToken("\"c\"")); // TODO
-			generateStatement.addDirective(Helpers.makeToken("gen"), expression);
-			instructions.add(generateStatement);
-			final LibraryStatementPart lsp = new LibraryStatementPartImpl();
-			lsp.setInstructions(instructions);
-//				lsp.setDirName();
-			final OS_Module module = om.success();
-			module.setLsp(lsp);
-
-			return Operation2.success(module);
-		} catch (final Exception e) {
-			errSink.exception(e);
-			return Operation2.failure(new ExceptionDiagnostic(e));
-		}
+		return new CY_FindPrelude(errSink, this).findPrelude(prelude_name);
 	}
 
 	private Operation2<OS_Module> parseElijjahFile(final @NotNull File f,
@@ -93,17 +144,26 @@ class USE {
 	                                               final @NotNull LibraryStatementPart lsp) {
 		System.out.printf("   %s%n", f.getAbsolutePath());
 
-		if (f.exists()) {
-			final Operation2<OS_Module> om = realParseElijjahFile2(file_name, f, do_out);
+		if (!f.exists()) {
+			final Diagnostic e = new FileNotFoundDiagnostic(f);
 
-			if (om.mode() == Mode.SUCCESS) {
+			return Operation2.failure(e);
+		}
+
+		Operation2<OS_Module> om;
+
+		try {
+			om = realParseElijjahFile(file_name, f, do_out);
+
+			switch (om.mode()) {
+			case SUCCESS -> {
 				final OS_Module mm = om.success();
 
 				//assert mm.getLsp() == null;
 				//assert mm.prelude == null;
 
 				if (mm.getLsp() == null) {
-					// TODO we dont know which prelude to find yet
+					// TODO we don't know which prelude to find yet
 					final Operation2<OS_Module> pl = findPrelude(Compilation.CompilationAlways.defaultPrelude());
 
 					// NOTE Go. infectious. tedious. also slightly lazy
@@ -112,16 +172,14 @@ class USE {
 					mm.setLsp(lsp);
 					mm.prelude = pl.success();
 				}
-
 				return Operation2.success(mm);
-			} else {
-				final Diagnostic e = new UnknownExceptionDiagnostic(om);
-				return Operation2.failure(e);
 			}
-		} else {
-			final Diagnostic e = new FileNotFoundDiagnostic(f);
-
-			return Operation2.failure(e);
+			default -> {
+				return om;
+			}
+			}
+		} catch (final Exception aE) {
+			return Operation2.failure(new ExceptionDiagnostic(aE));
 		}
 	}
 
@@ -137,40 +195,20 @@ class USE {
 //				final CompFactory.InputRequest inp = c.con().createInputRequest(file, do_out, lsp);
 
 				final String file_name = file.toString();
-				parseElijjahFile(file, file_name, do_out, lsp);
+				var          om        = parseElijjahFile(file, file_name, do_out, lsp);
+
+				assert om.mode() == Mode.SUCCESS;
 
 //				c.reports().addInput(inp, Finally.Out2.ELIJAH);
-				c.reports().addInput(()->file_name, Finally.Out2.ELIJAH);
+				c.reports().addInput(() -> file_name, Finally.Out2.ELIJAH);
 			}
 		}
 	}
 
-	public Operation2<OS_Module> realParseElijjahFile2(final String f, final @NotNull File file, final boolean do_out) {
-		final Operation<OS_Module> om;
-
-		try {
-			om = realParseElijjahFile(f, file, do_out);
-		} catch (final Exception aE) {
-			return Operation2.failure(new ExceptionDiagnostic(aE));
-		}
-
-		switch (om.mode()) {
-		case SUCCESS -> {
-			return Operation2.success(om.success());
-		}
-		case FAILURE -> {
-			final Exception e = om.failure();
-			errSink.exception(e);
-			return Operation2.failure(new ExceptionDiagnostic(e));
-		}
-		default -> throw new IllegalStateException("Unexpected value: " + om.mode());
-		}
-	}
-
-	public Operation<OS_Module> realParseElijjahFile(final String f, final @NotNull File file, final boolean do_out) throws Exception {
+	public Operation2<OS_Module> realParseElijjahFile(final String f, final @NotNull File file, final boolean do_out) throws Exception {
 		try (final InputStream s = c.getIO().readFile(file)) {
 			final ElijahSpec spec = new ElijahSpec(f, file, s, do_out);
-			return realParseElijjahFile(spec);
+			return Operation2.convert(realParseElijjahFile(spec));
 		}
 	}
 
