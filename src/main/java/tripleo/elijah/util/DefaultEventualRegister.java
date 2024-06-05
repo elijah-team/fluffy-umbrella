500f29525f07db043053bb0847f586657ed1dc42
Tripleo <noble@tripleo.nonexistent>
2024-06-05 03:20:01 -0400

    Eventual from congenial
    - https://github.com/elijah-team/congenial-robot/commit/85fea407f24272530ff731235d126153c37f6659

---
+++ src/main/java/tripleo/elijah/util/DefaultEventualRegister.java
@@ -0,0 +1,27 @@
+package tripleo.elijah.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DefaultEventualRegister implements EventualRegister {
+	final List<Eventual<?>> _eventuals = new ArrayList<Eventual<?>>();
+
+	public DefaultEventualRegister() {
+	}
+
+	@Override
+	public <P> void register(final Eventual<P> e) {
+		_eventuals.add(e);
+	}
+
+	@Override
+	public void checkFinishEventuals() {
+		int y = 0;
+		for (Eventual<?> eventual : _eventuals) {
+			if (eventual.isResolved()) {
+			} else {
+				System.err.println("[PipelineLogic::checkEventual] failed for " + eventual.description());
+			}
+		}
+	}
+}
